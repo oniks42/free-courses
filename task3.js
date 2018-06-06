53f748801ee419e0e66bde66b258b77923104155
oniks42 <39944632+oniks42@users.noreply.github.com>
2018-06-06 22:39:24 +0700

    Update task3.js

--- task3.js
+++ task3.js
@@ -9,7 +9,42 @@ var remainder = t % 3600;
 var minute = (remainder / 60 | 0);
 var second = t % 60;
 
-process.stdout.write(hour.toString() + ' час ' + minute.toString() + ' минут ' +
-    second.toString() + ' секунд');
+function write_hour(hour) {
+  if (hour === 0) {
+    //pass
+  } else if (hour === 1) {
+    process.stdout.write(hour.toString() + ' час ');
+  } else if ((hour%10 >= 2) && (hour%10 <= 4) && ((hour / 10 | 0) != 1)) {
+    process.stdout.write(hour.toString() + ' часа ');
+  } else {
+    process.stdout.write(hour.toString() + ' часов ');
+  }
+}
+
+function write_minute(minute) {
+  if (minute === 0) {
+    //pass
+  } else if (minute === 1) {
+    process.stdout.write(minute.toString() + ' минута ');
+  } else if ((minute%10 >= 2) && (minute%10 <= 4) && ((minute / 10 | 0) != 1)) {
+    process.stdout.write(minute.toString() + ' минуты ');
+  } else {
+    process.stdout.write(minute.toString() + ' минут ');
+  }
+}
+
+function write_second(second) {
+  if (second === 1) {
+    process.stdout.write(second.toString() + ' секунда');
+  } else if ((second%10 >= 2) && (second%10 <= 4) && ((second / 10 | 0) != 1)) {
+    process.stdout.write(second.toString() + ' секунды');
+  } else {
+    process.stdout.write(second.toString() + ' секунд');
+  }
+}
+
+write_hour(hour);
+write_minute(minute);
+write_second(second);
 
 /* Put your code here */
