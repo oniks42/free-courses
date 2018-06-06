aa55c49f8666c72d2f4edccdbe9371e281d2495a
oniks42 <39944632+oniks42@users.noreply.github.com>
2018-06-06 22:59:51 +0700

    Update task2.js

--- task2.js
+++ task2.js
@@ -8,15 +8,14 @@ var months = ['января', 'февраля', 'марта', 'апреля', '�
   'сентября', 'октября', 'ноября', 'декабря'];
 var day_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31];
 
-var day_in_year;
-if ((year % 4) && !(year % 100 )) {
-  day_in_year = 366;
+if (((year % 4) === 0) && !((year % 100 ) === 0)) {
+  day_in_month[1] = 29;
 } else {
-  day_in_year = 365;
+  day_in_month[1] = 28;
 }
 
 index = months.indexOf(month);
-var days = day_in_month[7] - day;
+var days = day_in_month[index] - day;
 i = index + 1;
 while (i < 12) {
   days += day_in_month[i];
@@ -24,5 +23,6 @@ while (i < 12) {
 }
 
 process.stdout.write(days.toString());
+
 
 /* Put your code here */
